7741953894bc23d99798fdcf4aba3048be0073c3
Nick Broglio <nickrbroglio@gmail.com>
2022-03-03 15:00:52 -0500

    changes

--- script.js
+++ script.js
@@ -1,5 +1,58 @@
-function weatherFetch(){
-    fetch("api.openweathermap.org/data/2.5/forecast?q=Trenton&appid=1b90528c11f2244a5b4f60002e540947")
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+// global variables
+
+// dom element references
+
+// function display search history
+function renderSearchHistory(){
+
 }
+
+// function update hisoty in local storage and update displayed history
+function appendToHistory(){
+
+}
+
+// function gte histroy fro local storage
+function initSearchHistory(){
+
+}
+
+// function display current weather from api
+function renderCurrentWeather(){
+
+}
+//  function display forecast given an object from weather api
+// daily forecast
+function renderForecastCard(){
+
+}
+
+// display five day forecast
+function renderForecast(){
+
+}
+
+// calls functions to display current weather data
+function renderItems(){
+
+}
+
+// fetches weather data from local from given from weather geo location 
+// then calls functions to display current weather data
+function fetchWeather(){
+
+}
+function fetchCoords(){
+
+}
+
+// event listener functions
+function handleSearchFormInit(){
+
+}
+function handleSearchHistoryClick(){
+
+}
+
+// create two submit buttons
+// search button and history button
